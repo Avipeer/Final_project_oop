7d7ab978507731e03439f46581b3c2174e740805
avipe <avipe@AVI>
2024-04-10 15:43:16 +0300

    Add project files.

---
+++ User.cpp
@@ -0,0 +1,52 @@
+﻿#include "User.h"
+
+
+void User::Print(int course_id) const {
+	if (course_id > 9999 && course_id < 100000) {
+		string f_name = "courses.csv";
+		int id = 11101;
+		char f[6] = {'1','1','1' ,'0' ,'1' ,'\0'};
+		string course;
+		course = f;
+		string fullname = course + "_yes";
+		fstream courses_f(f_name, ios::in);
+		if (!courses_f) {
+			throw("Cannot open file.\n");
+		}
+		//cout << "success";
+		string s;
+		int a;
+		courses_f >> a;
+		while (getline(courses_f, s)) {
+			if (a == course_id) {
+				cout << a << s << endl;
+				break;
+			}
+			else {
+				if (courses_f.eof()) {
+					cout << " Course id not found" << endl;
+					break;
+				}
+				courses_f >> a;
+
+			}
+		}
+
+	}
+	else {
+		cout << " Invalid course id" << endl;
+	}
+}
+//void User::PrintCourse() const {
+//
+//}
+//void User::More() const {
+//
+//}
+
+void main() {
+	fsteam current_file;
+	current_file >> a;
+	User a;
+	a.Print(11101);
+}
---
+++ User.h
@@ -0,0 +1,17 @@
+#ifndef _USER_H_
+#define _USER_H_
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+class User {
+public:
+	User() {};
+	void Print(int) const;
+	//void PrintCourse() const;
+	//void More() const;
+	~User() {};
+};
+
+
+#endif
